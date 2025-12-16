faa065f92893b8268d586cc8cb815946ddfd43f0
Swati <bhoyar.swati53@gmail.com>
2025-12-16 16:38:38 +1100

    schema validations

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const connectdb = require("./config/database");
 const {adminAuth} = require("./middlewares/auth");
-const  User = require("./models/user")
+const  User = require("./models/user");
+const { model } = require("mongoose");
 
 
 const app = express();
@@ -17,7 +18,75 @@ app.post("/signup", async (req, res)=>{
      } catch(error) {
           res.status(400).send("error occured while saving request"+ error.message);
      }
-    
+});
+
+//get user by email id
+app.get("/user", async (req, res) => {
+     const userEmail = req.query.emailId;
+     try {
+          console.log("userEmail=", userEmail);
+          const user = await User.find({ emailId: userEmail });
+          if (user.length === 0) {
+               res.status(404).send("User not found");
+          } else {
+               res.send(user);
+          }
+     } catch (err) {
+          res.status(404).send("not found" + err.message)
+     }
+});
+
+//Feed API -Get/feed - get all the users from database
+app.get("/feed", async (req, res) => {
+     try {
+          const feeds = await User.find({});
+          if (feeds.length === 0) {
+               res.status(404).send("There are no feeds");
+          } else {
+               res.send(feeds)
+          }
+     } catch (error) {
+          res.status(404).send("Error finding feeds" + error.message)
+     }
+});
+
+
+//Delete a user fromn database
+app.delete("/delete", async (req,res) => {
+    const userId = req.query._id;
+    console.log("id=="+ userId);
+   try {
+    await User.deleteOne({_id :userId});
+    res.send("deleted user");
+     
+   }catch(err) {
+     res.status(404).send("Error in deleting"+ err.message);
+   }
+});
+
+//Update data of the user (findByIdAndUpdate)
+app.patch("/update", async (req, res) => {
+     const id = req.query._id;
+     const data = req.body;
+
+     console.log("data=", data);
+     console.log("id=", id)
+
+     try {
+          const result = await User.findByIdAndUpdate(id, data, {runValidators: true});  //upadte first doc
+
+          if (!result) {
+               res.status(404).send("User not found..");
+          } else {
+               res.json({
+                    message: "User updated successfully!",
+                    result
+               })
+          }
+
+     } catch (error) {
+          res.status(500).send("update failed.." + error.message);
+     }
 });
 
 connectdb()
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,53 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    firstName: {
-        type: String
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            minLength: 4,
+            maxLength: 30
+        },
+        lastName: {
+            type: String
+        },
+        emailId: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        age: {
+            type: Number,
+            min: [18,"Minimun age should be 18"]
+        },
+        gender: {
+            type: String,
+            lowercase: true,
+            validate(value){
+                if(!(["male","female","others"].includes(value))){
+                    throw new Error("Gender data is not valid");
+                }
+            }
+        },
+        photoUrl: {
+           type: String ,
+           default: "https://www.freepik.com/free-vector/illustration-businessman_2606517.htm#fromView=keyword&page=1&position=30&uuid=04c2a359-a20e-4160-a3b4-da2f30b200ea&query=Default+profile"
+        },
+        about: {
+            type: String,
+            default: "This is my default about info"
+        }
     },
-    lastName: {
-        type: String
-    },
-    emailId: {
-        type: String
-    },
-    password: {
-        type: String
-    },
-    age: {
-        type: Number
-    },
-    gender: {
-        type: String
-    } 
-});
+    {
+        timestamps: true
+    }
+);
 
 module.exports = mongoose.model("User", userSchema);
 
